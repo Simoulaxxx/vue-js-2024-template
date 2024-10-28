25fd3971b5574c965f1665f5d57181e753bb13ae
simoulax <becanesimon@gmail.com>
2024-10-28 15:00:42 +0000

    simon

---
+++ src/assets/data.js
@@ -0,0 +1,90 @@
+export const sneakers = [
+  {
+    brand: 'Nike',
+    model: 'Air Max 90',
+    description:
+      "Une basket classique reconnue pour ses couleurs audacieuses et son coussin d'air visible au talon.",
+    release_date: '1990-03-26',
+    image_url:
+      'https://static.nike.com/a/images/t_default/080fe1e4-f422-4ed1-8dec-61d178a026f5/NIKE+AIR+MAX+90.png',
+  },
+  {
+    brand: 'Adidas',
+    model: 'Yeezy Boost 350 V2',
+    description:
+      'Une collaboration entre Adidas et Kanye West, connue pour son confort et son design unique.',
+    release_date: '2016-09-24',
+    image_url:
+      'https://cdn.shopify.com/s/files/1/2358/2817/products/yeezy-boost-350-v2-black-red-744518.png?v=1638814833',
+  },
+  {
+    brand: 'Puma',
+    model: 'Suede Classic',
+    description:
+      'Une sneaker emblématique avec une tige en daim lisse, populaire depuis les années 1960.',
+    release_date: '1968-01-01',
+    image_url:
+      'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/399781/01/sv01/fnd/EEA/fmt/png/Sneakers-Suede-Classic',
+  },
+  {
+    brand: 'Reebok',
+    model: 'Club C 85',
+    description:
+      'Une sneaker intemporelle inspirée du tennis, connue pour son style épuré et minimaliste.',
+    release_date: '1985-01-01',
+    image_url:
+      'https://i8.amplience.net/i/jpl/jd_619896_a?qlt=92&w=600&h=425&v=1&fmt=auto',
+  },
+  {
+    brand: 'New Balance',
+    model: '574',
+    description:
+      'Une chaussure de course populaire avec un design classique et des caractéristiques axées sur le confort.',
+    release_date: '1988-01-01',
+    image_url:
+      'https://img01.ztat.net/article/spp-media-p1/661a68beafbf47279eb8c48d37182077/909dcc9985ea40eb853ed66945bdaf69.jpg?imwidth=1800&filter=packshot',
+  },
+  {
+    brand: 'Nike',
+    model: 'Air Jordan 1',
+    description:
+      'La première sneaker Air Jordan qui a révolutionné le basketball et la culture des sneakers.',
+    release_date: '1985-04-01',
+    image_url:
+      'https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1b1785a7-b7b2-4b09-915f-257fd37b7653/AIR+JORDAN+1+MID.png',
+  },
+  {
+    brand: 'Converse',
+    model: 'Chuck Taylor All Star',
+    description:
+      'Une sneaker classique en toile, portée à travers les générations et les styles.',
+    release_date: '1917-01-01',
+    image_url:
+      'https://img01.ztat.net/article/spp-media-p1/c4f73dbf65774c8096e9a773cba6f77c/99254b00432b4829823c9d75cfe9fee2.jpg?imwidth=1800&filter=packshot',
+  },
+  {
+    brand: 'Asics',
+    model: 'Gel-Lyte III',
+    description:
+      'Une chaussure de course unique avec une languette fendue, reconnue pour son confort et son style.',
+    release_date: '1990-01-01',
+    image_url: 'https://i.ebayimg.com/images/g/xyAAAOSwixtXQ4dn/s-l1200.jpg',
+  },
+  {
+    brand: 'Vans',
+    model: 'Old Skool',
+    description:
+      'Une chaussure de skate classique avec la bande latérale emblématique, connue pour sa durabilité et son style.',
+    release_date: '1977-01-01',
+    image_url:
+      'https://static.ftshp.digital/img/p/1/1/6/7/7/3/0/1167730-full_product.jpg',
+  },
+  {
+    brand: 'Fila',
+    model: 'Disruptor II',
+    description:
+      'Une sneaker imposante devenue une icône de mode dans les années 90 et 2000.',
+    release_date: '1996-01-01',
+    image_url: 'https://i8.amplience.net/i/jpl/jd_711572_a?qlt=92',
+  },
+]
---
+++ src/views/page2View.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ItemSsneakers from '../components/ItemSsneakers.vue'
+import { sneakers } from '../assets/data.js'
+</script>
+
+<template>
+  <ItemCard company="sneakers" />
+</template>
